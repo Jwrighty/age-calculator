0efb8dedc3f4f6f3fcc50f00548eade1ca82c6b2
Jason Wright <jason.wright@vimeo.com>
2023-06-01 14:26:32 +0900

    Update app height

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { DurationObjectUnits } from "luxon";
 import { useState } from "react";
 
+import Counter from "./components/counter";
 import Form from "./components/form";
 import { usePrevious } from "./hooks";
-import Counter from "./components/counter";
 
 function App() {
   const [age, setAge] = useState<DurationObjectUnits>({
@@ -15,7 +15,7 @@ function App() {
   const prevAge = usePrevious(age);
 
   return (
-    <main className="flex h-screen w-screen items-center justify-center bg-slate-100 p-8">
+    <main className="flex h-full w-screen items-center justify-center bg-slate-100 p-8">
       <div className="flex w-full max-w-2xl flex-col gap-8 rounded-3xl rounded-br-[124px] bg-white px-8 py-12 sm:px-12">
         <Form setAge={setAge} />
         <div className="flex flex-col gap-2 text-5xl font-extrabold italic sm:text-7xl">
